e63f53ebb1787513fb3edc7601798b89041ed0b8
Man Yiu <11912398+manyiu@users.noreply.github.com>
2022-04-04 15:16:45 +0800

    add tailwindcss support

--- libraries/ui/.storybook/main.js
+++ libraries/ui/.storybook/main.js
@@ -1,13 +1,38 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  core: {
+    builder: "webpack5",
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "storybook-svgr-react-component",
+    // "storybook-svgr-react-component",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   features: {
     emotionAlias: false,
+  },
+  webpackFinal: (config) => {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      use: ["@svgr/webpack", "url-loader"],
+    });
+
+    return config;
   },
 };
